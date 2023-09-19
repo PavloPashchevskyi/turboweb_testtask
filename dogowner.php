0ec62b5837e9e46dc6c3746a43439bbc5327eb7e
Pavlo Pashchevskyi <googalltooth4@gmail.com>
2023-09-19 19:54:02 +0300

    Processing of custom exceptions has been added

--- dogowner.php
+++ dogowner.php
@@ -4,5 +4,9 @@ require_once 'vendor/autoload.php';
 
 use application\Application;
 
-Application::run($argc, $argv);
+try {
+    Application::run($argc, $argv);
+} catch (\Exception $e) {
+    echo "EXCEPTION#".$e->getCode().": ".$e->getMessage()."\n";
+}
 
